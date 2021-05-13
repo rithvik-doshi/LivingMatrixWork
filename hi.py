d60534e0395b2a1548b5dd607d2e562032581840
rithvik-doshi <81876806+rithvik-doshi@users.noreply.github.com>
2021-05-13 14:06:13 -0700

    Add files via upload

---
+++ hi.py
@@ -0,0 +1,8 @@
+#Here's a python file for you to run
+a = ['Oh','hi',"Here's", 'a', 'list', 'see', 'i', 'really', 'do', 'know', 'python']
+i=0
+for x in a:
+	print(x)
+	i+=1
+print(f"Wow i'm using a fancy formatted string to tell you that the number of items in that list is {i}")
+
